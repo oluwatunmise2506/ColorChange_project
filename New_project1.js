ed965530655fb78faec2403e5bb92b89d8f15797
oluwatunmise ikusika <oluwatunmisemary2506@gmail.com>
2022-09-20 15:29:16 +0100

    This is what i have done today

---
+++ New_project1.js
@@ -0,0 +1,11 @@
+function changecolor()
+ {
+    document.getElementById("myText").style.color = "red"
+    
+}
+
+function showText()
+{
+    document.getElementById("text").innerHTML = "learn something today"
+    
+}
