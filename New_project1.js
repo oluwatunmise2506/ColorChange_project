7107b9b3074f78261626f0c86d4dd7a14d4c94b3
oluwatunmise ikusika <oluwatunmisemary2506@gmail.com>
2022-09-22 22:35:51 +0100

    add changes

--- New_project1.js
+++ New_project1.js
@@ -9,3 +9,10 @@ function showText()
     document.getElementById("text").innerHTML = "learn something today"
     
 }
+
+let WildAnimals = ["Lions" , "Elephants" , "Leopards ", "Tigers"] ;
+document.getElementById("thisText").innerHTML = WildAnimals;
+WildAnimals[0] = "Buffalos";
+console.log(WildAnimals);
+console.log(WildAnimals.indexOf("Tigers", 1));
+document.getElementById("Mytext").innerHTML = "I love " +  WildAnimals[3] + " and " + WildAnimals[0];
