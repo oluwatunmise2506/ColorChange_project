0d5bb72e000fe01bd2ae286db880bd8c7ce9ceec
oluwatunmise ikusika <oluwatunmisemary2506@gmail.com>
2022-09-23 09:02:44 +0100

    added for loop

--- New_project1.js
+++ New_project1.js
@@ -16,3 +16,27 @@ WildAnimals[0] = "Buffalos";
 console.log(WildAnimals);
 console.log(WildAnimals.indexOf("Tigers", 1));
 document.getElementById("Mytext").innerHTML = "I love " +  WildAnimals[3] + " and " + WildAnimals[0];
+
+
+//loops
+
+//common loops
+
+//for
+//forof
+
+//while - while something is still happening, do this
+//dowhile
+
+let fruits = ["mango", "pineapple", "apple", "orange", "cashew", "pawpaw"]
+
+//for loop
+
+for (let index = 4; index > -1; index--) {
+    let arrayIndex = index;
+    let element = fruits[index];
+    console.log(element);
+    
+}
+
+
