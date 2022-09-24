41f911e3724cc0078e3d27d3467b53acb1c2ea5b
oluwatunmise ikusika <oluwatunmisemary2506@gmail.com>
2022-09-24 07:04:50 +0100

    I learnt more about JavaScript Array methods

--- New_project1.js
+++ New_project1.js
@@ -44,7 +44,7 @@ console.log("______________________________________________________________")
 for (let index = 0; index < fruits.length; index++) {
     const element = fruits[index];
     let mySentence = `${element} is the number ${index + 1} fruit in the array`
-    console.log(mySentence);
+    console.log(mySentence)
     
 }
 
@@ -56,5 +56,32 @@ for (const fruit of fruits) {
     if(fruit.includes("o") > 0){
         console.log(fruit.toUpperCase());
     }
-        
 }
+
+let people = ["Mother", "Sister", "father","Uncle", "Brother", "Aunt"]
+for (let i = 0; i < 6; i++) 
+{
+    const element = people[i];
+    let Thetext = "My " + element + " has a car" + "<br>"
+    console.log(Thetext);
+}
+
+let students =["Billy" , "Thomas" , "Yemi" , "Mary"]
+let ourText =""
+for (let i = 0; i < students.length; i++) {
+    const element = students[i];
+    document.getElementById("demo").innerHTML = ourText;
+}
+
+let food = ["Eba" , "Beans"]
+document.getElementById("favfood").innerHTML = food;
+
+food.push("Garri")
+document.getElementById("FavFood").innerHTML ="Is it for " + food
+console.log(food);
+console.log(Array.isArray(food));
+
+console.log("___________________________________________");
+
+food.pop()
+console.log(food);
